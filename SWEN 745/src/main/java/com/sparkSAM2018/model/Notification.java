48e43a030226a1513ec3c22d89942c57af4708df
Jeff Palmerino <31670860+jeffrey-palmerino@users.noreply.github.com>
2017-12-02 21:23:40 -0500

    Add files via upload

---
+++ SWEN 745/src/main/java/com/sparkSAM2018/model/Notification.java
@@ -0,0 +1,14 @@
+package com.sparkSAM2018.model;
+
+public class Notification {
+
+    private String message;
+
+    public Notification(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
